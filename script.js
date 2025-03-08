e4b44f89cf987d892ae7ab3ec93f2dd1805c3e5f
Arekusanda-desu <sasza.zahorodnii@gmail.com>
2025-03-08 20:48:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+function loadPage(url) {
+  fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      document.querySelector('main'). innerHTML = data;
+    })
+  catch(error => {
+    console.error('Ошибка при загрузке страницы:', error);
+  });
+}
+document.querySelectorAll('.cell a').forEach(link => {
+  link.addEventListener('click', finction (event) {
+    event.preventDefault();
+    const url = this.getAttribute('href');
+    loadPage(url);
+  });
+});
