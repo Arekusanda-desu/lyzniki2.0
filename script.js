20c8aba51e167192f50d72eac687f73a2163502d
Arekusanda-desu <sasza.zahorodnii@gmail.com>
2025-03-08 21:30:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function loadPage(url) {
   fetch(url)
-    .then(response => response.text())
-    .then(data => {
-      document.querySelector('main'). innerHTML = data;
-    })
-  catch(error => {
-    console.error('Ошибка при загрузке страницы:', error);
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Страница не найдена')
+      }
+      return response.text();
+  })
+  .then(data => {
+    document.querySelector('main').innerHTML = data;
+})
+.catch(error => {
+  console.error('Ошибка при загрузке страницы', error)
+  document.querySelector('main').innerHTML = '<p>Ошибка при загрузке страницы. Попробуйте позже.</p>';
+});
 }
 document.querySelectorAll('.cell a').forEach(link => {
-  link.addEventListener('click', finction (event) {
+  link.addEventListener('click', function (event) {
     event.preventDefault();
     const url = this.getAttribute('href');
     loadPage(url);
